891b6b25bcd0d8e5e1ee2320d40cfb6868b1bd27
Pianya <sleepypianocat@gmail.com>
2017-10-25 23:46:38 +0800

    read JSON

--- ping.py
+++ ping.py
@@ -1,7 +1,13 @@
 import json
 from pprint import pprint
 
-with open ('gui-config.json') as data_file:
+dir = 'D:\Program files Portable\Shadowsocks'
+
+with open (dir + '\gui-config.json') as data_file:
     data = json.load(data_file)
 
-pprint(data)
+test = data["configs"][0]["server"]
+
+ping(test)
+
+pprint(test)
