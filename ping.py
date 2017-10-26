cb9c73f21cbb1ac943e3d1e5103a22b9ace9bd48
Pianya <sleepypianocat@gmail.com>
2017-10-27 01:15:20 +0800

    call psping

--- ping.py
+++ ping.py
@@ -1,13 +1,17 @@
 import json
 from pprint import pprint
+import os
+import platform
 
 dir = 'D:\Program files Portable\Shadowsocks'
 
 with open (dir + '\gui-config.json') as data_file:
     data = json.load(data_file)
 
-test = data["configs"][0]["server"]
-
-ping(test)
-
-pprint(test)
+index = data["index"]
+pprint(index)
+i = 1
+ip = data["configs"][i]["server"]
+port = str(data["configs"][i]["server_port"])
+#pprint(ip + ':' + port)
+os.system('psping -t ' + ip + ':' + port)
