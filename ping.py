a6cb1720ac515b49dccd8da79088119afbcd2caf
Pianya <sleepypianocat@gmail.com>
2017-10-27 17:47:25 +0800

    add inputing dir

--- ping.py
+++ ping.py
@@ -3,15 +3,20 @@ from pprint import pprint
 import os
 import platform
 
-dir = 'D:\Program files Portable\Shadowsocks'
 
-with open (dir + '\gui-config.json') as data_file:
+dir = input('Enter the dir: ')
+
+with open (dir) as data_file:
     data = json.load(data_file)
 
 index = data["index"]
 pprint(index)
-i = 1
-ip = data["configs"][i]["server"]
-port = str(data["configs"][i]["server_port"])
-#pprint(ip + ':' + port)
-os.system('psping -t ' + ip + ':' + port)
+
+while (i < index):
+	ip = data["configs"][i]["server"]
+    port = str(data["configs"][i]["server_port"])
+    remark = data["configs"][i]["remarks"]
+    #pprint(ip + ':' + port)
+    pprint('request for server ' + remark)
+    os.system('psping -q -i 0 ' + ip + ':' + port)
+    i = i + 1
