38d3aa8314de273bf39154411bf5b9576009ec4a
Pianya <sleepypianocat@gmail.com>
2017-11-09 17:45:05 +0800

    for sync

--- ping.py
+++ ping.py
@@ -4,14 +4,6 @@ import os
 import platform
 import concurrent.futures
 
-# ask for dir: gui-config.json
-dir = input('Enter the dir: ')
-# load json
-with open (dir) as data_file:
-    data = json.load(data_file)
-# count no. of servers
-count = len(data['configs'])
-pprint(count)
 # function of call psping and miscellous
 def f(i):
     ip = data["configs"][i]["server"]
@@ -19,12 +11,25 @@ def f(i):
     remark = data["configs"][i]["remarks"]
     res = os.popen('psping -q -i 0 ' + ip + ':' + port + ' -nobanner').read()
     print('request for server ' + remark + '\n' + res)
-    #print('request for server ' + remark)
-# psping each server
-# lets try concurrent calling
-with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-    for i in range (0,count):
-        executor.submit(f,i)
-#for i in range (0,count):
-#    f(i)
-#    i = i + 1
+# main function    
+def main():
+    # ask for dir: gui-config.json
+    dir = input('Enter the dir: ')
+    # load json
+    with open (dir) as data_file:
+        data = json.load(data_file)
+    # count no. of servers
+    count = len(data['configs'])
+    pprint(count)
+    # psping each server
+    # lets try concurrent calling
+    # ThreadPool:
+    #with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    #   for i in range (0,count):
+    #        executor.submit(f,i)
+    # ProcessPool:
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for i in zip(range(0,count),executor.map(f,range(0,count)))
+        
+if __name__ == '__main__':
+    main()
